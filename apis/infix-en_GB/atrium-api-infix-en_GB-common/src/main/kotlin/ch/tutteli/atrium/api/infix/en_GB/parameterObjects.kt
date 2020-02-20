244188340ae898508315fd4730c8655dc3582594
jlundhol <jlundhol@kth.se>
2020-02-20 12:58:44 +0100

    Copy Pairs to infix parameterObjects

--- apis/infix-en_GB/atrium-api-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/parameterObjects.kt
+++ apis/infix-en_GB/atrium-api-infix-en_GB-common/src/main/kotlin/ch/tutteli/atrium/api/infix/en_GB/parameterObjects.kt
@@ -1,6 +1,17 @@
 package ch.tutteli.atrium.api.infix.en_GB
 
+import ch.tutteli.atrium.domain.builders.utils.VarArgHelper
+
 /**
  * Wrapper for a single index -- can be used as distinguishable type for an overload where Int is already in use.
  */
 data class Index(val index: Int)
+
+
+/**
+ * Parameter object to express `Pair<K, V>, vararg Pair<K, V>` in the infix-api.
+ */
+class Pairs<out K, out V>(
+    override val expected: Pair<K, V>,
+    override vararg val otherExpected: Pair<K, V>
+) : VarArgHelper<Pair<K, V>>
